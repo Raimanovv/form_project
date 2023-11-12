a5e8efe1668159f853fc91392ca37beae42eb127
rustam <rustam.raimanovv@gmail.com>
2023-11-12 13:35:32 +0500

    0001 - create fields without css

---
+++ feedback/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+
+class FeedbackForm(forms.Form):
+    name = forms.CharField(label='Имя', max_length=7, min_length=2, error_messages={
+        'max_length': 'Слишком много символов',
+        'min_length': 'Слишком мало символов',
+        'required': 'Поле не может быть пустым'
+    })
+    # surname = forms.CharField()
+    # feedback = forms.CharField(widget=forms.Textarea(attrs={'rows': 2, 'cols': 20}))
+    rating = forms.IntegerField(label='Рейтинг', max_value=5, min_value=1)
+
--- feedback/views.py
+++ feedback/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from .forms import FeedbackForm
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'feedback/feedback.html')
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponseRedirect('/done')
+    else:
+        form = FeedbackForm()
+    return render(request, 'feedback/feedback.html', context={'form': form})
+
+
+def done(request):
+    return render(request, 'feedback/done.html')
