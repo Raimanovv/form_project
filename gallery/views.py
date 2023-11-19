37a50144d502efd7d0c35c3572274352c28c8ec5
rustam <rustam.raimanovv@gmail.com>
2023-11-19 21:59:03 +0500

    0008 - Forms FileField

---
+++ gallery/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.views import View
+from .forms import GalleryUploadForm
+from django.http import HttpResponseRedirect
+
+
+def storage_file(file):
+    with open(f'gallery_tmp/{file.name}', 'wb+') as new_file:
+        for chunk in file.chunks():
+            new_file.write(chunk)
+
+
+class GalleryView(View):
+    def get(self, request):
+        form = GalleryUploadForm()
+        return render(request, 'gallery/load_file.html', context={'form': form})
+
+    def post(self, request):
+        form = GalleryUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            storage_file(request.FILES['image'])
+            return HttpResponseRedirect('load_image')
+        return render(request, 'gallery/load_file.html', context={'form': form})
+
