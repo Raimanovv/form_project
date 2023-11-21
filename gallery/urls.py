43e47e99eb60f7c35ff85f6417f3fb6b1a87c295
rustam <rustam.raimanovv@gmail.com>
2023-11-21 21:21:22 +0500

    0009 - Finish

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('load_image', views.GalleryView.as_view()),
+    path('load_image', views.CreateGalleryView.as_view()),
+    path('list_image', views.ListGallery.as_view()),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -2,23 +2,33 @@ from django.shortcuts import render
 from django.views import View
 from .forms import GalleryUploadForm
 from django.http import HttpResponseRedirect
+from .models import Gallery
+from django.views.generic.edit import CreateView
+from django.views.generic import ListView
 
 
-def storage_file(file):
-    with open(f'gallery_tmp/{file.name}', 'wb+') as new_file:
-        for chunk in file.chunks():
-            new_file.write(chunk)
+class ListGallery(ListView):
+    template_name = 'gallery/list_file.html'
+    model = Gallery
+    context_object_name = 'records'
 
 
-class GalleryView(View):
-    def get(self, request):
-        form = GalleryUploadForm()
-        return render(request, 'gallery/load_file.html', context={'form': form})
+class CreateGalleryView(CreateView):
+    model = Gallery
+    # fields = "__all__"
+    form_class = GalleryUploadForm
+    template_name = 'gallery/load_file.html'
+    success_url = 'load_image'
 
-    def post(self, request):
-        form = GalleryUploadForm(request.POST, request.FILES)
-        if form.is_valid():
-            storage_file(request.FILES['image'])
-            return HttpResponseRedirect('load_image')
-        return render(request, 'gallery/load_file.html', context={'form': form})
-
+# class GalleryView(View):
+#     def get(self, request):
+#         form = GalleryUploadForm()
+#         return render(request, 'gallery/load_file.html', context={'form': form})
+#
+#     def post(self, request):
+#         form = GalleryUploadForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             new_file = Gallery(image=request.FILES['image'])
+#             new_file.save()
+#             return HttpResponseRedirect('load_image')
+#         return render(request, 'gallery/load_file.html', context={'form': form})
